478ef428a62275bfae2c6c7b3e3aeab518d6ebef
nicklasbns <nicklasbns@gmail.com>
2022-05-06 01:03:10 +0200

    kinda behind on the synopsis... should have started sooner :suk:

---
+++ Programmerings kode.ts
@@ -0,0 +1,30 @@
+//word data
+
+true //true
+false //false
+
+5 > 2 //true
+8 < 6 //false
+14069 == 4 //false
+
+
+100
+75
+90911901919
+var a = 100;
+a //100
+
+BigInt(100); //100n
+BigInt("129048319048391048910348109348")+BigInt(1); //129048319048391048910348109349n
+
+"bogstaver"
+"string" + "string" //"stringstring"
+var navn = "Nicklas";
+var efternavn = "Østerberg";
+navn + " " + efternavn; //"Nicklas Østerberg"
+
+`${navn} ${efternavn}` //"Nicklas Østerberg"
+
+
+var person = {navn: "Nicklas", efternavn: "Østerberg", alder: 18, sej: true, sjovJoke: () => {return "What do you call a fish with no eyes? Fsh";}};
+
